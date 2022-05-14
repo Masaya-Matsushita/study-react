92107979c9954655bb35d40f4ec211db20a15f82
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-14 15:39:03 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書きました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,20 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import Head from "next/head"
 import styles from "src/styles/Home.module.css"
 import { Header } from "src/components/Header"
 import { Main } from "src/components/Main"
 import { Footer } from "src/components/Footer"
+import { useCallback } from "react/cjs/react.production.min"
 
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault()
+    alert(foo)
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +22,9 @@ export default function Home() {
       </Head>
 
       <Header />
-
+      <a href="/about" onClick={handleClick}>
+        a
+      </a>
       <Main page="index" />
 
       <Footer />
