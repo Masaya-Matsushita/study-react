23dd6edc8b3e9e8b532985bafa2abd47e436945a
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-19 16:13:47 +0900

    🚚 投稿系ページの名前変更

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,9 +1,8 @@
-import Head from "next/head"
 import classes from "src/styles/Home.module.css"
 import { Header } from "src/components/Header"
 import { Post } from "src/components/Post"
 
-const PostId = () => {
+const PostsId = () => {
   return (
     <div className={classes.container}>
       <Header />
@@ -12,4 +11,4 @@ const PostId = () => {
   )
 }
 
-export default PostId
+export default PostsId
