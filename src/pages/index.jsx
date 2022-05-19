9198c2f2f55ed1513a662cdf710de8bc6e1d699e
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-19 15:04:33 +0900

    🔥 Indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,19 @@
 import Head from "next/head"
 import classes from "src/styles/Home.module.css"
 import { Header } from "src/components/Header"
-import { Main } from "src/components/Main"
-import { Footer } from "src/components/Footer"
 
-const About = (props) => {
+const Index = (props) => {
   return (
     <div className={classes.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
 
       <Header />
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>
-        })}
-      </ul>
-      <button onClick={props.handleAdd}>追加</button>
-      <Main page="about" />
-
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIを色々叩いてみるよ!</p>
     </div>
   )
 }
 
-export default About
+export default Index
