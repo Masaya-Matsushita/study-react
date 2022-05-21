429f8f0982940f789f0448615cd43c6597c8c655
Masaya-Matsushita <Masaya.feb14@gmail.com>
2022-05-21 12:14:33 +0900

    ⚡️ コメント一覧のページをSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,9 +1,27 @@
-import { Comments as CommentsComponent} from "src/components/Comments"
+import { Comments as CommentsComponent } from "src/components/Comments"
+import { SWRConfig } from "swr"
 
-const Comments = () => {
+export const getStaticProps = async () => {
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`
+  const comments = await fetch(COMMENTS_API_URL)
+  const commentsData = await comments.json()
+
+  return {
+    props: {
+      fallback: {
+        [COMMENTS_API_URL]: commentsData,
+      },
+    },
+  }
+}
+
+const Comments = (props) => {
+  const { fallback } = props
   return (
     <div>
-      <CommentsComponent />
+      <SWRConfig value={{ fallback }}>
+        <CommentsComponent />
+      </SWRConfig>
     </div>
   )
 }
